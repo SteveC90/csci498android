09919553a472da40b8e3c464ed2efa85c2665a4a
Esteban Chavez <echavez@mymail.mines.edu>
2012-08-25 10:47:57 -0600

    Finished BCG chapter 7

--- HelloAndroid/src/csci498/hello/helloandroid/HelloActivity.java
+++ HelloAndroid/src/csci498/hello/helloandroid/HelloActivity.java
@@ -2,19 +2,23 @@ package csci498.hello.helloandroid;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-
-public class HelloActivity extends Activity {
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hello);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_hello, menu);
-        return true;
-    }
+import android.view.View;
+import android.widget.Button;
+import java.util.Date;
+public class HelloActivity extends Activity implements View.OnClickListener {
+	Button btn;
+	@Override
+	public void onCreate(Bundle icicle) {
+		super.onCreate(icicle);
+		btn=new Button(this);
+		btn.setOnClickListener(this);
+		updateTime();
+		setContentView(btn);
+	}
+	public void onClick(View view) {
+		updateTime();
+	}
+	private void updateTime() {
+		btn.setText(new Date().toString());
+	}
 }
