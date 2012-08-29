2561189bd1d0acb04549b059146a117e50e92e09
Esteban Chavez <echavez@mymail.mines.edu>
2012-08-28 19:32:27 -0600

    Supports all screen sizes and added the restaurant class.

---
+++ LunchList/src/csci498/echavez/lunchlist/Restaurant.java
@@ -0,0 +1,24 @@
+package csci498.echavez.lunchlist;
+
+public class Restaurant {
+	
+	private String name = "";
+	private String address = "";
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String n){
+		this.name = n;
+	}
+	
+	public String getAddress() {
+		return address;
+	}
+	
+	public void setAddress(String addr){
+		this.address = addr;
+	}
+	
+}
