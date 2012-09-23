7b6a910069133aae9161211513e16e826249db81
Esteban Chavez <echavez@mymail.mines.edu>
2012-09-23 16:15:59 -0600

    Updated manifest, modified onListClick listener, and created detail_form.xml

--- LunchList/src/csci498/echavez/lunchlist/MainActivity.java
+++ LunchList/src/csci498/echavez/lunchlist/MainActivity.java
@@ -23,6 +23,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.app.TabActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.widget.TabHost;
 import android.widget.AdapterView;
@@ -165,21 +166,9 @@ public class MainActivity extends TabActivity {
 	
 	private AdapterView.OnItemClickListener onListClick = new AdapterView.OnItemClickListener() {
 		public void onItemClick(AdapterView<?> parent, View view, int position, long id){
-			
-			model.moveToPosition(position);
-			name.setText(helper.getName(model));
-			address.setText(helper.getAddress(model));
-			notes.setText(helper.getNotes(model));
-			
-			if(helper.getType(model).equals("sit_down")){
-				types.check(R.id.sit_down);
-			} else if (helper.getType(model).equals("take_out")){
-				types.check(R.id.take_out);
-			} else {
-				types.check(R.id.delivery);
-			}
-			
-			getTabHost().setCurrentTab(1);
+			Intent i = new Intent(MainActivity.this, DetailForm.class);
+			startActivity(i);
+
 		}
 	};
 }
