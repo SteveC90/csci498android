b2f1ace99b6f485682a3afb6da4947fc2bf7b463
Esteban Chavez <echavez@mymail.mines.edu>
2012-09-28 21:27:33 -0600

    Created EditPreferences activity class and updated manifest. Also removed some unused libraries.

--- LunchList/src/csci498/echavez/lunchlist/RestaurantHelper.java
+++ LunchList/src/csci498/echavez/lunchlist/RestaurantHelper.java
@@ -1,10 +1,8 @@
 package csci498.echavez.lunchlist;
 
 import android.content.Context;
-import android.database.SQLException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteQueryBuilder;
 import android.database.Cursor;
 import android.content.ContentValues;
 
