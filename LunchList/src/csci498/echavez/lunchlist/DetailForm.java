a864ad29d65f0287ff4b7ec2c9a261de811e85dd
Esteban Chavez <echavez@mymail.mines.edu>
2012-09-30 19:08:47 -0600

    The form now has an text field to add a feed url. The RestaurantHelper database has been changed to reflect that. Also, labels have been removed in the detail_form.xml and in place, hints are used (for notes and feed text fields only).

--- LunchList/src/csci498/echavez/lunchlist/DetailForm.java
+++ LunchList/src/csci498/echavez/lunchlist/DetailForm.java
@@ -13,6 +13,7 @@ public class DetailForm extends Activity {
 	EditText name = null;
 	EditText address = null;
 	EditText notes = null;
+	EditText feed =null;
 	RadioGroup types = null;
 	RestaurantHelper helper = null;
 	String restaurantId = null;
@@ -27,6 +28,7 @@ public class DetailForm extends Activity {
         name = (EditText)findViewById(R.id.name);
         address = (EditText)findViewById(R.id.addr);
         notes = (EditText)findViewById(R.id.notes);
+        feed = (EditText)findViewById(R.id.feed);
         types = (RadioGroup)findViewById(R.id.types);
         
         Button save = (Button)findViewById(R.id.save);
@@ -72,6 +74,7 @@ public class DetailForm extends Activity {
 		name.setText(helper.getName(c));
 		address.setText(helper.getAddress(c));
 		notes.setText(helper.getNotes(c));
+		feed.setText(helper.getFeed(c));
 		
 		if (helper.getType(c).equals("sit_down")){
 			types.check(R.id.sit_down);
@@ -101,9 +104,9 @@ public class DetailForm extends Activity {
 			}
 			
 			if(restaurantId == null) {
-				helper.insert(name.getText().toString(), address.getText().toString(), type, notes.getText().toString());
+				helper.insert(name.getText().toString(), address.getText().toString(), type, notes.getText().toString(), feed.getText().toString());
 			} else {
-				helper.update(restaurantId, name.getText().toString(), address.getText().toString(), type, notes.getText().toString());
+				helper.update(restaurantId, name.getText().toString(), address.getText().toString(), type, notes.getText().toString(), feed.getText().toString());
 			}
 			
 			finish();
