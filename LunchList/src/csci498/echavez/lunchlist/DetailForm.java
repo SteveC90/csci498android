eec296c5c3e912fc497389f3b1eb97610e0d4bc2
Esteban Chavez <echavez@mymail.mines.edu>
2012-11-03 23:27:22 -0600

    Added the marker overlay for the map. The RestaurantMap uses an ItemizedOverlay subclass to place markers on discrete points

--- LunchList/src/csci498/echavez/lunchlist/DetailForm.java
+++ LunchList/src/csci498/echavez/lunchlist/DetailForm.java
@@ -29,6 +29,9 @@ public class DetailForm extends Activity {
 	TextView location = null;
 	LocationManager locMgr = null;
 	
+	double latitude = 0.0d;
+	double longitude = 0.0d;
+	
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -108,6 +111,10 @@ public class DetailForm extends Activity {
 			return true;
 		} else if (item.getItemId() == R.id.map){
 			Intent i = new Intent(this, RestaurantMap.class);
+			i.putExtra(RestaurantMap.EXTRA_LATITUDE, latitude);
+			i.putExtra(RestaurantMap.EXTRA_LONGITUDE, longitude);
+			i.putExtra(RestaurantMap.EXTRA_NAME, name.getText().toString());
+			
 			startActivity(i);
 			return true;
 		}
@@ -148,6 +155,9 @@ public class DetailForm extends Activity {
 		} else {
 			types.check(R.id.take_out);
 		}
+		
+		latitude = helper.getLatitude(c);
+		longitude = helper.getLongitude(c);
 		
 		location.setText(String.valueOf(helper.getLatitude(c))+", "+String.valueOf(helper.getLongitude(c)));
 		
